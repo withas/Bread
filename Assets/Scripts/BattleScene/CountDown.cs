66dcb530b6b6ef7f0761bc3c025979921804bccd
hinata <with.as.net@gmail.com>
2022-12-10 02:18:17 +0900

    CountDownをコルーチンからUniTaskを返すメソッドに変更した

--- Assets/Scripts/BattleScene/CountDown.cs
+++ Assets/Scripts/BattleScene/CountDown.cs
@@ -1,82 +1,42 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using SelectCharacter;
+using Cysharp.Threading.Tasks;
 
 public class CountDown : MonoBehaviour
 {
-    [SerializeField] Text textCountdown;
-    [SerializeField] int CountDownNum = 3;
-    [SerializeField] GameObject[] player;
-    GameStarter gameStart;
+    [SerializeField]
+    private Text textCountdown;
+
+    [SerializeField]
+    private AudioSource audioSource;
 
     //開始のコング
-    [SerializeField] AudioClip startSE;
-    AudioSource audioSource;
+    [SerializeField]
+    private AudioClip startSE;
 
-    // private GameManagerDate gameManagerData;  //キャラクターを保存しているデータ
+    [SerializeField]
+    private int countDownNum = 3;
 
-
-    void Start()
-    {
-        audioSource = gameObject.GetComponent<AudioSource>();
-        gameStart = FindObjectOfType<GameStarter>();
-
-        player = new GameObject[2];
-        // gameManagerData = GameObject.Find("GameManager").GetComponent<GameManagerDate>();
-        player[0] = gameStart.activeChara[0];
-        player[1] = gameStart.activeChara[1];
-
-
-        // //テスト用
-        // if(player[0] == null) 
-        // {
-        //     player[0] = GameObject.Find("Player1");
-        //     player[0].SetActive(true);
-        // }
-        // //ここまでテスト用
-
-        textCountdown.text = "";
-        foreach (var p in player)
-        {
-            p.GetComponent<PlayerController>().enabled = false;
-        }
-
-        StartCoroutine(CountdownCoroutine());
-    }
-
-    IEnumerator CountdownCoroutine()
+    public async UniTask CountDownAsync()
     {
         textCountdown.gameObject.SetActive(true);
 
-
-        // textCountdown.text = "3";
-        // yield return new WaitForSeconds(1.0f);
-
-        // textCountdown.text = "2";
-        // yield return new WaitForSeconds(1.0f);
-
-        // textCountdown.text = "1";
-        // yield return new WaitForSeconds(1.0f);
-
-        //カウントダウン開始
-        while (CountDownNum > 0)
+        // カウントダウン開始
+        while (countDownNum > 0)
         {
-            textCountdown.text = CountDownNum.ToString("F0");
-            yield return new WaitForSeconds(1.0f);
-            CountDownNum--;
+            textCountdown.text = countDownNum.ToString("F0");
+
+            await UniTask.Delay(1000);
+
+            countDownNum--;
         }
+
         textCountdown.text = "GO!";
         audioSource.PlayOneShot(startSE);
-        yield return new WaitForSeconds(1.0f);
+
+        await UniTask.Delay(1000);
 
         textCountdown.text = "";
         textCountdown.gameObject.SetActive(false);
-
-        foreach (var p in player)
-        {
-            p.GetComponent<PlayerController>().enabled = true;
-        }
     }
 }
