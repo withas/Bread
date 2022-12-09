8513da00710b9d2584ebc348097a5330d4182036
hinata <with.as.net@gmail.com>
2022-12-09 22:44:07 +0900

    Characters.csにコメントを追加した

--- Assets/Scripts/Characters.cs
+++ Assets/Scripts/Characters.cs
@@ -1,7 +1,17 @@
+/// <summary>
+/// キャラクターの種類
+/// </summary>
 public enum Characters
 {
+    // カレーパン
     Curry,
+
+    // フランスパン
     France,
+
+    // メロンパン
     Melon,
+
+    // チョココロネ
     Cornet,
 }
