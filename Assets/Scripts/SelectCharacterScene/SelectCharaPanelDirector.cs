dd35cf4b161633e6235ac47d12f12f15f6e1defa
hinata <with.as.net@gmail.com>
2022-12-10 00:40:24 +0900

    add SelectCharaPanelDirector.cs

---
+++ Assets/Scripts/SelectCharacterScene/SelectCharaPanelDirector.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+
+public sealed class SelectCharaPanelDirector : MonoBehaviour
+{
+    [SerializeField]
+    private StatusPanelDirector statusPanelDirector;
+
+    [SerializeField]
+    private Button selectCurryButton;
+
+    [SerializeField]
+    private Button selectFranceButton;
+
+    [SerializeField]
+    private Button selectMelonButton;
+
+    [SerializeField]
+    private Button selectCornetButton;
+
+    private void Start()
+    {
+        selectCurryButton.OnClickAsObservable()
+                         .Select(_ => Characters.Curry)
+                         .Subscribe(OnSelectCharaButtonClicked)
+                         .AddTo(this);
+
+        selectFranceButton.OnClickAsObservable()
+                          .Select(_ => Characters.France)
+                          .Subscribe(OnSelectCharaButtonClicked)
+                          .AddTo(this);
+
+        selectMelonButton.OnClickAsObservable()
+                         .Select(_ => Characters.Melon)
+                         .Subscribe(OnSelectCharaButtonClicked)
+                         .AddTo(this);
+
+        selectCornetButton.OnClickAsObservable()
+                          .Select(_ => Characters.Cornet)
+                          .Subscribe(OnSelectCharaButtonClicked)
+                          .AddTo(this);
+    }
+
+    private void OnSelectCharaButtonClicked(Characters character)
+    {
+        statusPanelDirector.SetStatus(character);
+    }
+}
