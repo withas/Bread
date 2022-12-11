374d986876b5ea9e48a6f320aaefdbcfc31c4e69
hinata <with.as.net@gmail.com>
2022-12-11 14:22:15 +0900

    キャラクター選択のステータス表示時をCharaStatusDataを参照するように変更した

--- Assets/Scripts/SelectCharacterScene/StatusPanelDirector.cs
+++ Assets/Scripts/SelectCharacterScene/StatusPanelDirector.cs
@@ -4,7 +4,7 @@ using UnityEngine.UI;
 public sealed class StatusPanelDirector : MonoBehaviour
 {
     [SerializeField]
-    private CharaPrefabsData charaPrefabsData;
+    private CharaStatusDictionary charaStatusDictionary;
 
     [SerializeField]
     private Slider hpSlider;
@@ -24,39 +24,20 @@ public sealed class StatusPanelDirector : MonoBehaviour
     public void SetStatus(Characters character)
     {
         // キャラクターのPrefabを取得する
-        if (!charaPrefabsData.TryGetPrefab(character, out var playerController))
+        if (!charaStatusDictionary.TryGetStatusData(character, out var statusData))
         {
             return;
         }
 
         // HP、Speed、Jumpスライダーの値を更新する
-        hpSlider.value = playerController.GetMaxHp();
-        speedSlider.value = playerController.GetSpeed();
-        jumpSlider.value = playerController.GetJump();
+        hpSlider.value = statusData.MaxHP;
+        speedSlider.value = statusData.MoveSpeed;
+        jumpSlider.value = statusData.JumpForce;
 
-        // Attack1のPowerを取得する
-        var punchController = playerController.GetComponentInChildren<PunchController>();
-        int attack1Power = punchController.GetPower();
-
-        // Attack2のPowerを取得する
-        int attack2Power = GetAttack2Power(character);
-
-        // Attackスライダーに各攻撃力の平均値を設定する
-        attackSlider.value = (attack1Power + attack2Power) / 2.0f;
+        // Attackスライダーに各攻撃の威力の平均値を設定する
+        attackSlider.value = (statusData.Attack1Power + statusData.Attack2Power) / 2.0f;
 
         // Guardスライダーにガード率の逆数を設定する
-        guardSlider.value = 1.0f / playerController.GetGuardingRatio();
-    }
-
-    private int GetAttack2Power(Characters character)
-    {
-        return character switch
-        {
-            Characters.Curry => charaPrefabsData.CurryAttack2Prefab.GetPower(),
-            Characters.France => charaPrefabsData.FranceAttack2Prefab.GetPower(),
-            Characters.Melon => charaPrefabsData.MelonAttack2Prefab.GetPower(),
-            Characters.Cornet => charaPrefabsData.CornetAttack2Prefab.GetPower(),
-            _ => 0,
-        };
+        guardSlider.value = 1.0f / statusData.GuardingRatio;
     }
 }
