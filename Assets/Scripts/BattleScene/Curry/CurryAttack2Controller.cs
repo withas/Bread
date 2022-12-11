da2fee0529bcef3e148e33f69934a139199084e3
hinata <with.as.net@gmail.com>
2022-12-11 15:05:14 +0900

    format CurryAttack2Controller.cs

--- Assets/Scripts/BattleScene/Curry/CurryAttack2Controller.cs
+++ Assets/Scripts/BattleScene/Curry/CurryAttack2Controller.cs
@@ -1,25 +1,26 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class CurryAttack2Controller : MonoBehaviour {
+public sealed class CurryAttack2Controller : MonoBehaviour
+{
     // 技の性能
-    [SerializeField] private int power = 5; // 威力
-    [SerializeField] private float freezingTime = 0.8f; // 硬直時間
+    [SerializeField]
+    private int power = 5; // 威力
 
-    public int GetPower() { return this.power; }
+    [SerializeField]
+    private float freezingTime = 0.8f; // 硬直時間
 
     // アニメーションの最後に呼び出す
-    public void AnimationEnd() {
+    public void AnimationEnd()
+    {
         this.transform.parent.gameObject.GetComponent<CurryController>().EndAttack2();
         Destroy(gameObject);
     }
 
-    private void OnTriggerEnter2D(Collider2D other) {
-        GameObject otherObject = other.gameObject;
-
-        if (otherObject.tag == "Player") {
-            PlayerController hitPlayer = otherObject.GetComponent<PlayerController>();
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        var otherObject = other.gameObject;
+        if (otherObject.tag == "Player" && otherObject.TryGetComponent<PlayerController>(out var hitPlayer))
+        {
             hitPlayer.OnDamage(this.power, this.freezingTime);
         }
     }
