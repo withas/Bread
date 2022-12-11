696b18d92ff3fbaaadc609fe093aef8b866ac695
hinata <with.as.net@gmail.com>
2022-12-11 15:01:17 +0900

    PunchControllerの各パラメータをCharaStatusDataから読み込むように変更した

--- Assets/Scripts/BattleScene/PunchController.cs
+++ Assets/Scripts/BattleScene/PunchController.cs
@@ -4,17 +4,14 @@ public sealed class PunchController : MonoBehaviour
 {
     // 技の性能
     [SerializeField]
-    private int power = 10; // 威力
-
-    [SerializeField]
-    private float freezingTime = 0.3f; // 硬直時間
+    private CharaStatusData charaStatusData;
 
     private void OnCollisionEnter2D(Collision2D other)
     {
         var otherObject = other.gameObject;
         if (otherObject.tag == "Player" && otherObject.TryGetComponent<PlayerController>(out var hitPlayer))
         {
-            hitPlayer.OnDamage(this.power, this.freezingTime);
+            hitPlayer.OnDamage(charaStatusData.Attack1Power, charaStatusData.Attack1FreezingTime);
         }
     }
 }
