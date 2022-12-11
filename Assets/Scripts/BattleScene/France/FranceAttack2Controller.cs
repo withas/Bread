c55153ffbe9450048d2ca75678946ef1a676a79f
hinata <with.as.net@gmail.com>
2022-12-11 15:25:04 +0900

    format FranceAttack2Controller.cs

--- Assets/Scripts/BattleScene/France/FranceAttack2Controller.cs
+++ Assets/Scripts/BattleScene/France/FranceAttack2Controller.cs
@@ -1,25 +1,27 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class FranceAttack2Controller : MonoBehaviour {
+public sealed class FranceAttack2Controller : MonoBehaviour
+{
     // 技の性能
-    [SerializeField] private int power = 5; // 威力
-    [SerializeField] private float freezingTime = 0.8f; // 硬直時間
+    [SerializeField]
+    private int power = 5; // 威力
 
-    public int GetPower() { return this.power; }
+    [SerializeField]
+    private float freezingTime = 0.8f; // 硬直時間
 
     // アニメーションの最後に呼び出す
-    public void AnimationEnd() {
+    public void AnimationEnd()
+    {
         this.transform.parent.gameObject.GetComponent<FranceController>().EndAttack2();
         Destroy(gameObject);
     }
 
-    private void OnCollisionEnter2D(Collision2D other) {
-        GameObject otherObject = other.gameObject;
-
-        if (otherObject.tag == "Player" && otherObject.transform != this.transform.parent) {
-            PlayerController hitPlayer = otherObject.GetComponent<PlayerController>();
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        var otherObject = other.gameObject;
+        if (otherObject.tag == "Player" && !otherObject.transform.Equals(transform.parent) &&
+            otherObject.TryGetComponent<PlayerController>(out var hitPlayer))
+        {
             hitPlayer.OnDamage(this.power, this.freezingTime);
         }
     }
